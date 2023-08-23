555cb8b16453c491b8d9135411a35b953a45caac
zelex <zelexshaw@163.com>
2023-08-23 22:25:19 +0800

    init prog

---
+++ src/main/java/com/zelex/starter/checker/CheckerSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.zelex.starter.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckerSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckerSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zelex/starter/checker/CheckerSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelex.starter.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckerSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
